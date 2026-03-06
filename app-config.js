a27c8f5b30da0238fd56e17e2a0903bed3a9e91b
lifetime790-lgtm <lifetime790@gmail.com>
2026-03-06 15:37:18 +0900

    Remove facility zones levels

--- app-config.js
+++ app-config.js
@@ -8,8 +8,6 @@ window.APP_CONFIG = {
   // 시설 맞춤 옵션 (필요 시 값 수정)
   facilityOptions: {
     sites: ["G-Cube V4-2", "G-Cube V4-1", "G-Cube V3", "중산재배기"],
-    zones: ["A", "B"],
-    levels: ["1F", "2F", "3F", "4F"],
     cropTypes: ["시금치", "상추류", "마이크로그린", "기타"],
     stages: ["준비", "파종", "발아/암발아", "육묘", "정식", "재배", "수확", "세척/소독", "점검/보수"],
   },
